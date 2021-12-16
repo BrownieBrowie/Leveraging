0ae57e48ed84b996ef02a5dca86f9dee49f0fd99
BrownieBrowie <93504059+BrownieBrowie@users.noreply.github.com>
2021-12-16 15:09:41 -0800

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -2,15 +2,37 @@
 #include <random>
 #include <algorithm>
 #include <string>
-#include "Utility.h"
-#include "Car.h"
+#include "Utility1.h"
+#include "Car1.h"
 
-bool compCars(const Car& c1, const Car& c2) {
-    return c1.price() >= c2.price();
-}
 int main() {
     auto cars = randomCars();
-    std::cout << "Initial: " << cars << "\n";
-    sort(cars.begin(), cars.end(), compCars);
-    std::cout << "Reversed: " << cars << std::endl;
+    std::cout << "Cars: " << cars << "\n";
+
+    unsigned long totalCars = cars.size();
+    double average = 0;
+    
+    double min = INT32_MAX;
+    double max = INT32_MIN;
+
+    for (auto& car : cars) {
+        double price = car.price();
+        max = std::max(max, price);
+        min = std::min(min, price);
+        average += car.price();
+    }
+    average /= totalCars;
+    double range = max - min;
+
+    for (auto& car : cars) {
+        double price = car.price();
+        max = std::max(max, price);
+        min = std::max(min, price);
+        average += car.price();
+    }
+    average /= totalCars;
+    double range = max - min;
+
+    std::cout << "The average price is: $" << average << "\n";
+    std::cout << "The range is: $" << range << "\n";
 }
